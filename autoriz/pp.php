6fc9852f11184dbaade83f2dbdc219582b6ba24f
unknown <pochebutov2013@gmail.com>
2022-12-10 05:48:16 +0700

    Database has been added

--- autoriz/pp.php
+++ autoriz/pp.php
@@ -3,6 +3,65 @@ session_start();
 header('Content-Type: text/html; charset= utf-8');
 require_once '../functions/connection.php';
 $link = mysqli_connect($host, $user, $password, "testing");
-
-include "../html/personal-page.html"
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/personal.css" />
+    <title>Document</title>
+  </head>
+  <body>
+    <header>
+      <div class="header">
+        <h1 class="PersonalAccount">Личный кабинет</h1>
+      </div>
+      <div class="MainContainer">
+        <div class="SupContainer">
+          <div class="SupContainer1">
+          <div class='tab'>
+    <?php
+          if(isset($_GET['dob_id'])){
+        $sql = mysqli_query($link, "SELECT id, login, email FROM t_users");
+		$result = mysqli_fetch_array($sql);
+        $date = date('Y-m-d');
+        echo '<p>{$result['id']}</p>';
+        $sql = mysqli_query($link, "INSERT INTO orders (customer_id, usl_id) VALUES ('{$result['id']}', '$usl_id')");
+        if($sql){
+            echo "<p>Заказ добавлен.</p>";
+        } else {
+            echo '<p>Произошла ошибка: '.mysqli_error($link).'</p>';
+        }
+    }
+    ?>
+        <table border='1' >
+     <l>Таблица услуг</l>
+         <br>
+            <tr>
+                <td>Идентификатор</td>
+                <td>Наименование</td>
+                <td>Цена</td>
+                <td>Приобрести</td>
+            </tr>
+    <?php 
+        $sql=mysqli_query($link, "SELECT id, title, price FROM uslugi");
+        while($result = mysqli_fetch_array($sql)){
+            $usl_id = $result['id'];
+            echo "<tr>
+                <td>{$result['id']}</td>
+                <td>{$result['title']}</td>
+                <td>{$result['price']} ₽</td>
+                <td><a href='?dob_id={$result['id']}'>Выбрать</a></td>
+                </tr>";
+        }
+    ?>
+        </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </body>
+</html>
